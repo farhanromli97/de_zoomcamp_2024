d22742a3b845967974f7b361df4709806927cc64
farhanromli <farhan.romli@outlook.com>
2024-02-01 17:32:38 +0800

    update mage terraform

---
+++ week_2_mage/mage-ai-terraform-templates/gcp/variables.tf
@@ -0,0 +1,77 @@
+variable "credentials" {
+  description = "My Credentials"
+  default     = "./keys/my-creds.json"
+  #ex: if you have a directory where this file is called keys with your service account json file
+  #saved there as my-creds.json you could use default = "./keys/my-creds.json"
+}
+
+
+variable "app_name" {
+  type        = string
+  description = "Application Name"
+  default     = "mage-data-prep"
+}
+
+variable "container_cpu" {
+  description = "Container cpu"
+  default     = "2000m"
+}
+
+variable "container_memory" {
+  description = "Container memory"
+  default     = "2G"
+}
+
+variable "project_id" {
+  type        = string
+  description = "The name of the project"
+  default     = "dtc-de-course-412112"
+}
+
+variable "region" {
+  type        = string
+  description = "The default compute region"
+  default     = "us-west2"
+}
+
+variable "zone" {
+  type        = string
+  description = "The default compute zone"
+  default     = "us-west2-a"
+}
+
+variable "repository" {
+  type        = string
+  description = "The name of the Artifact Registry repository to be created"
+  default     = "mage-data-prep"
+}
+
+variable "database_user" {
+  type        = string
+  description = "The username of the Postgres database."
+  default     = "mageuser"
+}
+
+variable "database_password" {
+  type        = string
+  description = "The password of the Postgres database."
+  sensitive   = true
+}
+
+variable "docker_image" {
+  type        = string
+  description = "The docker image to deploy to Cloud Run."
+  default     = "mageai/mageai:latest"
+}
+
+variable "domain" {
+  description = "Domain name to run the load balancer on. Used if `ssl` is `true`."
+  type        = string
+  default     = ""
+}
+
+variable "ssl" {
+  description = "Run load balancer on HTTPS and provision managed certificate with provided `domain`."
+  type        = bool
+  default     = false
+}
